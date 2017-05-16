67b11fefc0622de6b433e31a9924e4bf5ac2364e
Gen Ichino <zenoplex@gmail.com>
2017-05-16 10:44:45 +0900

    feat: add pusher settings in api

---
+++ lib/env.ts
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+export const PUSHER_APP_ID: string = process.env.PUSHER_APP_ID || '';
+export const PUSHER_KEY: string = process.env.PUSHER_KEY || '';
+export const PUSHER_SECRET: string = process.env.PUSHER_SECRET || '';
--- lib/routes/api.ts
+++ lib/routes/api.ts
@@ -1,11 +1,18 @@
 import * as R from 'ramda';
 import * as express from 'express';
+import Pusher = require('pusher');
+import * as env from '../env';
 
 const router: express.Router = express.Router();
+const pusher: Pusher = new Pusher({ 
+  appId: env.PUSHER_APP_ID,
+  key: env.PUSHER_KEY,
+  secret: env.PUSHER_KEY,
+});
 
 const hello
 : (router: express.Router) => express.Router
-= router => router.get('/', (req, res) => res.send('all good'));
+= router => router.get('/', (req, res) => res.send('It works'));
 
 const pusherAuth
 : (router: express.Router) => express.Router
