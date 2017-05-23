21902e57a406da387eea418dd602c0ae675af84c
Gen Ichino <zenoplex@gmail.com>
2017-05-24 01:04:36 +0900

    feat: add PUSHER_CLUSTER env variable

--- lib/env.ts
+++ lib/env.ts
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
-const { PUSHER_APP_ID, PUSHER_KEY, PUSHER_SECRET } = process.env;
+const { PUSHER_APP_ID, PUSHER_KEY, PUSHER_SECRET, PUSHER_CLUSTER } = process.env;
 
 export const pusherAppId: string = PUSHER_APP_ID || '';
 export const pusherKey: string = PUSHER_KEY || '';
 export const pusherSecret: string = PUSHER_SECRET || '';
+export const pusherCluster: string = PUSHER_CLUSTER || '';
