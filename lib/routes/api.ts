8afd00d8ccf7a3316b13ed34d6aecc5e706c552d
Gen Ichino <zenoplex@gmail.com>
2017-05-26 02:17:22 +0900

    fix: pusherKey to pusherSecret

--- lib/routes/api.ts
+++ lib/routes/api.ts
@@ -7,25 +7,20 @@ const router: express.Router = express.Router();
 const pusher: Pusher = new Pusher({ 
   appId: env.pusherAppId,
   key: env.pusherKey,
-  secret: env.pusherKey,
+  secret: env.pusherSecret,
   cluster: env.pusherCluster,
   encrypted: true,
 });
 
-const hello
-: (router: express.Router) => express.Router
-= router => router.get('/', (req, res) => res.send('It works'));
-
 const pusherAuth
 : (router: express.Router) => express.Router
 = router => router.post('/pusher/auth', (req, res) => {
-  const { socket_id, channel } = req.body;
-  res.send(pusher.authenticate(socket_id, channel));
+  const { socket_id, channel_name } = req.body;
+  res.send(pusher.authenticate(socket_id, channel_name));
 });
 
 export const api
 : (app: express.Application) => express.Application
 = app => app.use('/', R.pipe(
-  hello,
   pusherAuth,
 )(router));
